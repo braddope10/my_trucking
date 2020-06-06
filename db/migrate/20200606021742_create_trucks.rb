7ab815094766ec4e71ffdbcf4b9198cc0c329624
github username <github email address>
2020-06-05 19:27:20 -0700

    trucks table

--- db/migrate/20200606021742_create_trucks.rb
+++ db/migrate/20200606021742_create_trucks.rb
@@ -1,6 +1,12 @@
 class CreateTrucks < ActiveRecord::Migration[6.0]
   def change
     create_table :trucks do |t|
+      t.string :make
+      t.stirng :model
+      t.string :year
+      t.string :color
+
+      t.timestamps
     end
   end
 end
