0d0c127a768a89c2a89611cb102f562512d238a5
github username <github email address>
2020-06-05 19:54:01 -0700

    dates table

--- db/migrate/20200606021808_create_brokers.rb
+++ db/migrate/20200606021808_create_brokers.rb
@@ -1,6 +1,9 @@
 class CreateBrokers < ActiveRecord::Migration[6.0]
   def change
     create_table :brokers do |t|
+      t.string :name
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20200606021824_create_dates.rb
+++ db/migrate/20200606021824_create_dates.rb
@@ -1,6 +1,9 @@
 class CreateDates < ActiveRecord::Migration[6.0]
   def change
     create_table :dates do |t|
+      t.string :time
+
+      t.timestamps
     end
   end
 end
