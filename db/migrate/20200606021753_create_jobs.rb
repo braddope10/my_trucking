108a8c5bbc0117663e53e52e0cb2d206730a77af
github username <github email address>
2020-06-05 19:48:51 -0700

    jobs table

--- db/migrate/20200606021753_create_jobs.rb
+++ db/migrate/20200606021753_create_jobs.rb
@@ -1,6 +1,19 @@
 class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
+      t.string :start_time
+      t.string :end_time
+      t.string :material
+      t.string :weight
+      t.string :dropoff
+      t.string :pickup
+      t.string :driver
+      t.string :contact_name
+      t.integer :contact_number 
+      t.integer :hourly_rate
+      t.integer :po_number
+
+      t.timestamps
     end
   end
 end
