668fc2d51fc8ef7d014cfec4e306be8ed8701003
github username <github email address>
2020-06-08 21:34:01 -0700

    user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
 
+    def new
+
+    end
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+        
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
 
