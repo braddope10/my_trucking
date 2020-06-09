523df0eb73f58eefc8be38844ffe8fd6d840e7c5
github username <github email address>
2020-06-08 21:45:18 -0700

    giving a session a user_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            
+            session[:user_id] = @user.id
         else
             render :new
         end
