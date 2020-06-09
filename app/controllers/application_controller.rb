6332fe34bb097d7ffa2727d5bc1bcb506bdb6cb3
github username <github email address>
2020-06-08 22:48:56 -0700

    finished methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
     helper_method :create_session
 
     def require_login
-        
+        redirect_to login_path unless session[:user_id]
     end
 
     def logged_in?
-        
+        session[:user_id] ? true : false
     end
 
     def create_session
