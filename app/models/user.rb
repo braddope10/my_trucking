2042cde1cc64beea6cee81e43ddaf129b2611c47
github username <github email address>
2020-06-08 19:39:30 -0700

    created validations & security measures

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
     has_many :brokers, :through => :jobs
     has_many :dates
 
+    validates :username, uniqueness: true, presence: true, length: { minimum: 5 }
+    validates :email, uniqueness: true, presence: true
+    validates :password, length: { in: 6..18 }
+
+    has_secure_password
+
 end
