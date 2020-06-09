82f3cb1ffe15ccc31fb43c1665a134e5c02ad8bd
github username <github email address>
2020-06-08 22:38:00 -0700

    create_session method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,16 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?
     helper_method :create_session
 
+    def require_login
+
+    end
+
     def logged_in?
         
     end
 
     def create_session
-
+        session[:user_id] = @user.id
     end
 
 end
